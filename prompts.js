6f65d24aedd617d01c42b14c490cae0fe278c2c5
huangiyongqi <860029955@qq.com>
2020-05-25 14:24:30 +0800

    添加prompts预

--- prompts.js
+++ prompts.js
@@ -1,1 +1,49 @@
-module.exports = []
+module.exports = [
+  {
+    name: 'cssPreprocessor',
+    type: 'list',
+    message: 'Add support for CSS pre-processors like Sass or Less',
+    choices: [
+      {
+        name: 'Less',
+        value: 'less'
+      },
+      {
+        name: 'Sass/SCSS',
+        value: 'sass'
+      }
+    ],
+    default: 'less'
+  },
+  {
+    name: 'Vue Router mode',
+    type: 'list',
+    message: 'Choice Vue Router History mode',
+    choices: [
+      {
+        name: 'Hash',
+        value: 'hash'
+      },
+      {
+        name: 'history',
+        value: 'history'
+      }
+    ],
+    default: 'hash'
+  },
+  {
+    name: 'Typescript',
+    type: 'confirm',
+    message: 'Add support for the TypeScript language (default: None)',
+    default: false
+  },
+  {
+    name: 'Server Port',
+    type: 'input',
+    message: 'Input DevServer Port(default: 8081)',
+    default: '8081',
+    validate: (n) => {
+      return !isNaN(+n)
+    }
+  }
+]
