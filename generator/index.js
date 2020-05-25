8ddd9c4a32e1811d70e9be69273604d20422969f
huangiyongqi <860029955@qq.com>
2020-05-25 15:08:26 +0800

    add generator config

--- generator/index.js
+++ generator/index.js
@@ -32,19 +32,24 @@ module.exports = (api, options, rootOptions) => {
     scripts: {
       "serve": "vue-cli-service serve",
       "build": "vue-cli-service build",
-      "lint": "vue-cli-service lint"
+      "lint": "vue-cli-service lint",
+      "extendPackage": 'vue-cli-service serve --copy',
     },
   });
 
   // 安装一些基础公共库
   api.extendPackage({
     dependencies: {
-      "core-js": "^3.6.4",
       "terser-webpack-plugin": "^2.3.5",
       "vue": "^2.6.11",
       "vue-router": "^3.1.5",
-      "vuex": "^3.1.2"
+      "vuex": "^3.1.2",
+      'axios': '^0.18.0',
+      'babel-polyfill': '^6.22.0',
+      'lodash': '^4.17.11',
+      "echarts": "^4.2.0-rc.1",
     },
+    // 开发依赖包
     devDependencies: {
       "@vue/cli-plugin-babel": "~4.2.0",
       "@vue/cli-plugin-eslint": "~4.2.0",
@@ -56,13 +61,47 @@ module.exports = (api, options, rootOptions) => {
       "eslint": "^6.7.2",
       "eslint-plugin-prettier": "^3.1.1",
       "eslint-plugin-vue": "^6.1.2",
-      "less": "^3.0.4",
-      "less-loader": "^5.0.0",
+      // "less": "^3.0.4",
+      // "less-loader": "^5.0.0",
       "lint-staged": "^9.5.0",
       "prettier": "^1.19.1",
       "vue-template-compiler": "^2.6.11"
     }
   });
+
+  // # less
+  if (opts['cssPreprocessor'] === 'less') {
+    api.extendPackage({
+      devDependencies: {
+        "less": "^2.7.2",
+        "less-loader": "^3.0.0"
+      }
+    })
+  }
+
+  // # sass
+  if (opts['cssPreprocessor'] === 'sass') {
+    api.extendPackage({
+      devDependencies: {
+        "node-sass": "^4.9.3",
+        "sass-loader": "^7.1.0"
+      }
+    })
+  }
+
+  // 扩展 .eslintrc 配置
+  api.extendPackage({
+    eslintConfig: {
+      "rules": {
+        "vue/no-parsing-error": [
+          2,
+          { "x-invalid-end-tag": false }
+        ]
+      }
+    }
+  })
+
+
   // 删除 vue-cli3 默认目录
   api.render(files => {
     Object.keys(files)
@@ -70,7 +109,10 @@ module.exports = (api, options, rootOptions) => {
       .forEach(path => delete files[path])
     console.log(Object.keys(files))
   })
+
+  // 生成模版（如果配ssr，渲染的位置可能会不同）
   api.render('../template');
+
   api.onCreateComplete(() => {
     process.env.VUE_CLI_SKIP_WRITE = true;
   });
