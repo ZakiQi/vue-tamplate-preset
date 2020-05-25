e80be74463fbe23b39d2d9b655fc1eefceba5b8c
huangiyongqi <860029955@qq.com>
2020-05-25 15:15:10 +0800

    Delete things that don‘t work

--- generator/index.js
+++ generator/index.js
@@ -1,32 +1,32 @@
-const fs = require('fs');
-const tool = (api) => {
-  return {
-    deleteFile(path) {
-      const file = api.resolve(path);
-      if (fs.existsSync(file)) {
-        fs.unlinkSync(file);
-      }
-    },
-    deleteDir(path) {
-      const dir = api.resolve(path);
-      if (fs.existsSync(dir)) {
-        fs.readdirSync(dir).forEach((o) => {
-          const file = dir + '\\' + o;
-          if (fs.statSync(file).isDirectory()) {
-            fs.readdirSync(dir).forEach((p) => {
-              fs.unlinkSync(dir + '\\' + o + '\\' + p);
-            });
-          } else {
-            fs.unlinkSync(file);
-          }
-        });
-        fs.rmdirSync(dir);
-      }
-    }
-  };
-};
-module.exports = (api, options, rootOptions) => {
-  const utils = tool(api);
+// const fs = require('fs');
+// const tool = (api) => {
+//   return {
+//     deleteFile(path) {
+//       const file = api.resolve(path);
+//       if (fs.existsSync(file)) {
+//         fs.unlinkSync(file);
+//       }
+//     },
+//     deleteDir(path) {
+//       const dir = api.resolve(path);
+//       if (fs.existsSync(dir)) {
+//         fs.readdirSync(dir).forEach((o) => {
+//           const file = dir + '\\' + o;
+//           if (fs.statSync(file).isDirectory()) {
+//             fs.readdirSync(dir).forEach((p) => {
+//               fs.unlinkSync(dir + '\\' + o + '\\' + p);
+//             });
+//           } else {
+//             fs.unlinkSync(file);
+//           }
+//         });
+//         fs.rmdirSync(dir);
+//       }
+//     }
+//   };
+// };
+module.exports = (api, opts, rootOpts) => {
+  // const utils = tool(api);
   // 命令
   api.extendPackage({
     scripts: {
